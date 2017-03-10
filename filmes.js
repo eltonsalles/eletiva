5197f7ae7be78c13e00da6ddc248a1d20a8b05d9
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-03-10 11:51:24 -0300

    Adicionado condição para quando o botão for todos e colocado alguns comentários

--- filmes.js
+++ filmes.js
@@ -1,22 +1,42 @@
+// Pega a section dos botões
 var btn = document.getElementById("botoes");
+
+// Percorre todos os filhos da variável btn (section dos botões)
 for (var i = 0; i < btn.children.length; i++) {
+	// Adiciona uma função ao evento click dos botões
 	document.getElementsByClassName("btn")[i].addEventListener("click", function() {
+		// Pega o id do botão que serve para saber em qual categoria o usuário clicou
 		var categoria = this.id.split("-")[1];
 
+		// Pega todos os filmes (cada filme está dentro de um article)
 		var filmes = document.getElementsByTagName("article");
+
+		// Percorre todos os filmes
 		for (var j = 0; j < filmes.length; j++) {
-			var filtrar = true;
+			// Remove a classe filtrar para que apareça todos os filmes antes de filtrar novamente
+			filmes[j].classList.remove("filtrar");
 
-			var categorias = filmes[j].classList;
-			for (var k = 0; k < categorias.length; k++) {
-				if (categorias[k] == categoria) {
-					filtrar = false;
-					break;
+			// Se a categoria do botão for todos então não faz mais nada
+			if (categoria != "todos") {
+				var filtrar = true;
+
+				// Pega todas as categorias do respectivo filme
+				var categorias = filmes[j].classList;
+
+				// Percorre as categiras
+				for (var k = 0; k < categorias.length; k++) {
+					// Se a categoria existir então o filme não deve ser filtrado
+					if (categorias[k] == categoria) {
+						// Define que filme não deve ser filtrado
+						filtrar = false;
+						break;
+					}
+				};
+
+				// Se filtrar ainda for verdadeiro, então o filme não tem a categoria do botão e precisa ser filtrado
+				if (filtrar) {
+					filmes[j].classList.toggle("filtrar");
 				}
-			};
-
-			if (filtrar) {
-				filmes[j].classList.toggle("filtrar");
 			}
 		};
 	})
