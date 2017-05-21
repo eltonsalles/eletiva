fdeaed9b6592c49c7c87276ac9719fce5473e311
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-05-21 15:30:00 -0300

    Feito a servlet que vai cuidar da conversa

--- Aula10/ChatJSON/src/java/EnvioMensagem.java
+++ Aula10/ChatJSON/src/java/EnvioMensagem.java
@@ -17,69 +17,36 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class EnvioMensagem extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EnvioMensagem</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EnvioMensagem at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    @Override
+    protected void service(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("get")) {
+            if (request.getParameter("conversa") != null
+                    && request.getParameter("texto") != null) {
+                String conversa = request.getParameter("conversa");
+                String texto = request.getParameter("texto");
+                Usuario usuario = (Usuario) request.getSession()
+                        .getAttribute("eu");
+                
+                String j = this.json(conversa, texto, usuario.getUsername());
+                
+                response.setContentType("text/json;charset=UTF-8");
+                try (PrintWriter out = response.getWriter()) {
+                    out.print("[");
+                    out.print(j);
+                    out.print("]");
+                }                
+            }
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    private String json(String conversa, String texto, String usuario) {
+        String json = "{";
+        json += "\"conversa\":\""+conversa+"\", ";
+        json += "\"usuario\":\""+usuario+"\", ";
+        json += "\"texto\":\""+texto+"\"";
+        json += "}";
+        
+        return json;
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
